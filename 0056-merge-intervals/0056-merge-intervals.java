4b124c1e57296fe347e19fdd94281971632cafc6
avi02iter <95306567+avi02iter@users.noreply.github.com>
2022-10-12 20:42:12 +0530

    Time: 15 ms (64.16%), Space: 55.8 MB (11.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,29 @@
 class Solution {
-
-   
-     public int[][] merge(int[][] intervals) {
-        List<int[]> answer = new ArrayList<>();
+    public int[][] merge(int[][] intervals) {
+       ArrayList<int[]> li=new ArrayList<>();
+        if(intervals.length==0||intervals==null){
+            return li.toArray(new int[0][]);
+        }
+        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+        int start=intervals[0][0];
+        int end=intervals[0][1];
         
-        if(intervals.length != 0 || intervals != null){
-            Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-            
-            int start = intervals[0][0];
-            int end = intervals[0][1];
-            for(int[] i: intervals){
-                if(i[0] <= end){
-                    end = Math.max(end, i[1]);
-                } else {
-                    answer.add(new int[]{start,end});
-                    start = i[0];
-                    end = i[1];
-                }
+        for(int[] i:intervals){
+            if(i[0]<=end){    //if less then merge by extending its end point
+                end=Math.max(end,i[1]);
+                
+            }else{
+                li.add(new int[]{start,end});
+                start=i[0];
+                end=i[1];
+                
+                
+                
             }
-            answer.add(new int[]{start,end}); 
             
         }
         
-        return answer.toArray(new int[0][]); 
+    li.add(new int[]{start,end});  //adding the last merge becouse we end from the for each loop at last
+        return li.toArray(new int[0][]);
     }
-        
-    
 }
