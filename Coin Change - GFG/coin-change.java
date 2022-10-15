97ae908c48d4e6b208fa41b0df3b5d97a92e74f4
avi02iter <95306567+avi02iter@users.noreply.github.com>
2022-10-15 21:47:34 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -27,20 +27,48 @@ class GfG {
 class Solution {
     public long count(int coins[], int N, int sum) {
        //tabulation
-       long[][] dp=new long[N+1][sum+1];
-       for(int i=0;i<N+1;i++){
-           for(int j=0;j<sum+1;j++){
-               if(i<=0&&j!=0){
-                   dp[i][j]=0;
-               }else if(j==0){
-                   dp[i][j]=1;
-               }else if(coins[i-1]<=j){
-                   dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
-               }else {
-                   dp[i][j]=dp[i-1][j];
-               }
-           }
-       }
-       return dp[N][sum];
+     long[][] dp=new long[N+1][sum+1];
+    //   for(int i=0;i<N+1;i++){
+    //       for(int j=0;j<sum+1;j++){
+    //           if(i<=0&&j!=0){
+    //               dp[i][j]=0;
+    //           }else if(j==0){
+    //               dp[i][j]=1;
+    //           }else if(coins[i-1]<=j){
+    //               dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+    //           }else {
+    //               dp[i][j]=dp[i-1][j];
+    //           }
+    //       }
+    //   }
+    //   return dp[N][sum];
+    
+    //memo
+    for(long[] row:dp){
+        Arrays.fill(row,-1);
+    }
+    
+    return memo(coins,N,sum,dp);
+    }
+    
+    
+    public long memo(int[] coins,int N,int sum,long[][] dp){
+        if(N<=0&&sum!=0){
+            return 0;
+        }
+        if(sum==0){
+            return 1;
+        }
+        if(dp[N][sum]!=-1){
+        return dp[N][sum];
+    }
+    
+    if(coins[N-1]<=sum){
+        dp[N][sum]=memo(coins,N,sum-coins[N-1],dp)+memo(coins,N-1,sum,dp);
+    }else{
+        dp[N][sum]=memo(coins,N-1,sum,dp);
+    }
+        return dp[N][sum];
+        
     }
 }
