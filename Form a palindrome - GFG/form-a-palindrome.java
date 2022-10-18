d53087fd4c756cebeacccd6ff2c551d09597cf88
Aaradhy srivastava <95306567+avi02iter@users.noreply.github.com>
2022-10-19 02:21:15 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String str = read.readLine();
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.countMin(str));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int countMin(String str)
+    {
+        int n=str.length();
+        String s="";
+        for(int i=n-1;i>=0;i--){
+            s+=str.charAt(i);
+        }
+        int m=s.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }else if(str.charAt(i-1)==s.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][m];
+    }
+}
