acfaf4a058632a7b72dc21eeac93aba31df553c6
Aaradhy srivastava <95306567+avi02iter@users.noreply.github.com>
2022-11-09 00:53:07 +0530

    Time: 12 ms (6.45%), Space: 41.7 MB (74.96%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,21 +1,34 @@
 class Solution {
 
     public boolean checkIfPangram(String sentence) {
-        boolean[] a = new boolean[26];
-        Arrays.fill(a, false);
+//         boolean[] a = new boolean[26];
+//         Arrays.fill(a, false);
 
-        for (int i = 0; i < sentence.length(); i++) {
-            char c = sentence.charAt(i);
-            int pos = c - 'a';
-            a[pos++] = true;
+//         for (int i = 0; i < sentence.length(); i++) {
+//             char c = sentence.charAt(i);
+//             int pos = c - 'a';
+//             a[pos++] = true;
+//         }
+
+//         for (int i = 0; i < a.length; i++) {
+//             if (a[i] == false) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+        
+        HashSet<Character> h=new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            h.add(sentence.charAt(i));
         }
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == false) {
-                return false;
-            }
+        
+        if(h.size()!=26){
+            return false;
         }
-
+        
         return true;
+        
+        
     }
 }
