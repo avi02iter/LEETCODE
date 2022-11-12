759da8b9f8fa50ab9618543e124924c87f8d1a1f
Aaradhy srivastava <95306567+avi02iter@users.noreply.github.com>
2022-11-12 22:48:20 +0530

    Time: 1 ms (88.84%), Space: 41.7 MB (96.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -3,23 +3,35 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return cost(cost, dp, n);
-    }
-
-    public int cost(int[] cost, int[] dp, int n) {
-        if (n == 0 || n == 1) {
-            return 0;
+        // Arrays.fill(dp, -1);
+        // return cost(cost, dp, n);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            dp[i]=Math.min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+            
         }
-        if (n < 0) {
-            return 0;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-
-        dp[n] = Math.min(cost[n - 1] + cost(cost, dp, n - 1), cost[n - 2] + cost(cost, dp, n - 2));
-
+        
+        
         return dp[n];
     }
+    
+    
+    //memoization
+
+//     public int cost(int[] cost, int[] dp, int n) {
+//         if (n == 0 || n == 1) {
+//             return 0;
+//         }
+//         if (n < 0) {
+//             return 0;
+//         }
+//         if (dp[n] != -1) {
+//             return dp[n];
+//         }
+
+//         dp[n] = Math.min(cost[n - 1] + cost(cost, dp, n - 1), cost[n - 2] + cost(cost, dp, n - 2));
+
+//         return dp[n];
+//     }
 }
