c65d42ee01c420c851eeae3eac80ef3a73394a67
Aaradhy srivastava <95306567+avi02iter@users.noreply.github.com>
2022-11-25 19:32:23 +0530

    Time: 3 ms (64.39%), Space: 42.3 MB (68.24%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,14 +1,15 @@
 class Solution {
+
     public String truncateSentence(String s, int k) {
-        String[] ar=s.split(" ");
-        String str=" ";
-        for(int i=0;i<k;i++){
-            str+=ar[i];
-            str+=" ";
+        String[] ar = s.split(" ");
+        //String str=" ";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < k-1; i++) {
+            sb.append(ar[i]+" ");
         }
         
-        return str.trim();
-        
-        
+        sb.append(ar[k-1]);
+
+        return sb.toString();
     }
 }
