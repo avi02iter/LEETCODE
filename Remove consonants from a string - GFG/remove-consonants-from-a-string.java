c19b5b7b91671d07d0eb2ddb1f63ffa6f592158b
Aaradhy srivastava <95306567+avi02iter@users.noreply.github.com>
2023-04-29 00:33:26 +0530

    Added solution - LeetHub

--- Remove consonants from a string - GFG/remove-consonants-from-a-string.java
+++ Remove consonants from a string - GFG/remove-consonants-from-a-string.java
@@ -26,19 +26,24 @@ class Solution
 {
     public String removeConsonants(String s)
     {
-        StringBuilder sb = new StringBuilder();
-        String str = s.toLowerCase();
-        
-        for(int i=0; i<s.length(); i++){
-            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'
-            ||str.charAt(i)=='u'){
-                sb.append(s.charAt(i));
+        StringBuilder a= new StringBuilder();
+         
+         for(int i=0;i<s.length();i++)
+         
+         {
+            if(s.charAt(i)=='a'|| s.charAt(i)=='e'|| s.charAt(i)=='i' ||s.charAt(i)=='o'|| s.charAt(i)=='u' ||s.charAt(i)=='A' ||s.charAt(i)=='I' ||s.charAt(i)=='O' ||s.charAt(i)=='E' ||s.charAt(i)=='U')
+            {
+                a.append(s.charAt(i));
+               
             }
-        }
-        if(sb.length()==0){
-            return "No Vowel";
-        }
-        return sb.toString();
+             
+            
+         }
+         if(a.length()==0)
+         {
+             return "No Vowel";
+         }
+          return a.toString();
     }
     
 }
